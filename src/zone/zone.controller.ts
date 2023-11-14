128679fc7d6ec4a08c00f13c6dce66657fe88f3e
alonespace <jomcootle@gmail.com>
2023-11-15 00:22:06 +0700

    add: zone controller

--- src/zone/zone.controller.ts
+++ src/zone/zone.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ZoneService } from './zone.service';
 import { CreateZoneDto } from './dto/create-zone.dto';
@@ -20,6 +21,7 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { CreateZoneResponse } from './dto/response/create-zone.response';
+import JwtAuthGuard from 'src/auth/jwt/jwt-auth.guard';
 
 @Controller('zones')
 @ApiTags('Zones')
@@ -33,6 +35,7 @@ export class ZoneController {
     description: 'Zone created successfully',
     type: CreateZoneResponse,
   })
+  @UseGuards(JwtAuthGuard)
   create(@Body() createZoneDto: CreateZoneDto) {
     return this.zoneService.create(createZoneDto);
   }
@@ -41,6 +44,7 @@ export class ZoneController {
   @ApiOperation({ summary: '(Zone) Get all zones' })
   @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
   @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
+  @UseGuards(JwtAuthGuard)
   findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
     return this.zoneService.findAll({ page: +page, limit: +limit });
   }
@@ -48,6 +52,7 @@ export class ZoneController {
   @Get(':id')
   @ApiOperation({ summary: '(Zone) Get a zone by id' })
   @ApiParam({ name: 'id', required: true, type: Number, example: 1 })
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.zoneService.findOne(+id);
   }
@@ -56,6 +61,7 @@ export class ZoneController {
   @ApiOperation({ summary: '(Zone) Update a zone by id' })
   @ApiParam({ name: 'id', required: true, type: Number, example: 1 })
   @ApiBody({ type: UpdateZoneDto })
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateZoneDto: UpdateZoneDto) {
     return this.zoneService.update(+id, updateZoneDto);
   }
