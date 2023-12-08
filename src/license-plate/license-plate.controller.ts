5d623540d4e0feab4247ab1ae8a92d87f1f104cc
Thiraphat <few.thiraphat@gmail.com>
2023-12-08 16:09:47 +0700

    filter search on licneseplate

--- src/license-plate/license-plate.controller.ts
+++ src/license-plate/license-plate.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
 import { ApiBody, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { InputLicensePlateDto } from "./dtos/license-plate.dto";
 import { LicensePlateService } from "./license-plate.service";
@@ -16,7 +16,13 @@ export class LicensePlateController {
     }
 
     @Get()
-    findAll() {
-      return this.licensePlateService.get();
+    findAll(
+      @Query('page') page: number = 1,
+      @Query('limit') limit: number = 10,
+      @Query('search') search: string,
+      @Query('zoneId') zoneId: number,
+    ) {
+      return this.licensePlateService.get({ page, limit, search, zoneId });
     }
+
 }
