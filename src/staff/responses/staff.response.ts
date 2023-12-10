78b3a16dc95d369511fd6e95a545fcff0ced0a7d
alonespace <jomcootle@gmail.com>
2023-12-10 14:39:35 +0700

    add: staff response

---
+++ src/staff/responses/staff.response.ts
@@ -0,0 +1,56 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class StaffResponse {
+  @ApiProperty({
+    description: 'The id of the Staff',
+    type: Number,
+    example: 1,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+
+  @ApiProperty({
+    description: 'The firstname of the Staff',
+    type: String,
+    example: 'John',
+  })
+  @IsString()
+  @IsNotEmpty()
+  firstname: string;
+
+  @ApiProperty({
+    description: 'The lastname of the Staff',
+    type: String,
+    example: 'Doe',
+  })
+  @IsString()
+  @IsNotEmpty()
+  lastname: string;
+
+  @ApiProperty({
+    description: 'The email of the Staff',
+    type: String,
+    example: 'johe.doe@gmail.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'The phone number of the Staff',
+    type: String,
+    example: '0912345678',
+  })
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @ApiProperty({
+    description: 'Status of using system',
+    type: Boolean,
+    example: true,
+  })
+  status: boolean;
+}
