76dd44f78b430f76ba6a5c9246f2afd56a057b99
alonespace <jomcootle@gmail.com>
2023-12-10 14:30:36 +0700

    add: swagger in upload controller

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,15 +1,56 @@
 // your.controller.ts
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('upload')
+@ApiTags('Upload file to AWS S3')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post()
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({ summary: 'Store file into S3' })
+  @ApiBody({
+    description: 'Upload file to S3',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'file',
+        },
+      },
+    },
+  })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Upload file to S3',
+    schema: {
+      type: 'object',
+      properties: {
+        url: {
+          type: 'string',
+          format: 'url',
+        },
+      },
+    },
+  })
   @UseInterceptors(FileInterceptor('file')) // 'file' should match the field name in your form
-  async uploadFileToS3(@UploadedFile() file: Express.Multer.File): Promise<{ url: string }> {
+  async uploadFileToS3(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<{ url: string }> {
     const bucketName = 'parkwise-kmutt';
     const key = 'x5r4';
 
